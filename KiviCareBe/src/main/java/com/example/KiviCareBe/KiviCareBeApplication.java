b0d9bf2bd6291caa9153e30f24d2aee49b3a5ec1
HuyDau <lehuydau2312@gmail.com>
2025-05-26 18:13:00 +0700

    Create new project

---
+++ KiviCareBe/src/main/java/com/example/KiviCareBe/KiviCareBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.KiviCareBe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiviCareBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiviCareBeApplication.class, args);
+	}
+
+}
